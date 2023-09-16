049e8cb5a974d2a53f1179e585549f8a5660a014
Mirofff <miroslavzherenkov@outlook.com>
2023-09-16 03:41:05 +0200

    add: login page and logic, also refactor user model logics

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,5 +1,12 @@
 FROM php:fpm-bullseye
 RUN docker-php-ext-install pdo pdo_mysql
 RUN docker-php-ext-enable pdo pdo_mysql
+# install xdebug
 RUN pecl install xdebug
+
+# copy over the config files
+COPY ./xdebug.ini /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+COPY ./error_reporting.ini /usr/local/etc/php/conf.d/error_reporting.ini
+
+# enable the extension
 RUN docker-php-ext-enable xdebug
--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
+require_once 'vendor/autoload.php';
+require_once 'routes.php';
 
-require "includes/header.php";
+$dotenv = Dotenv\Dotenv::createImmutable('.');
+$dotenv->load();
 
-echo "WTF nigga??";
+require_once "Common/Auth.php";
+
+spl_autoload_register(function($className)
+{
+    $className=str_replace("\\","/",$className);
+    $class = (empty($namespace)?"":$namespace."/")."{$className}.php";
+    include_once($class);
+});
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // Обработка случая, когда маршрут не найден
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        // Обработка случая, когда метод запроса не разрешен
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $params = $routeInfo[2];
+
+        list($controllerName, $methodName) = explode('@', $handler);
+        $controller = new $controllerName();
+        $controller->$methodName($params);
+        break;
+}
